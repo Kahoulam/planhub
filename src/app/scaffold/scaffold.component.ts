94b30a8671b3f7fbf423df1f83faf2345bf294f4
neslxzhen <neslxzhen@gmail.com>
2020-08-26 09:09:37 +0800

    feat: search tags

--- src/app/scaffold/scaffold.component.ts
+++ src/app/scaffold/scaffold.component.ts
@@ -6,41 +6,56 @@ import { NotifyService } from '../notify.service';
 import { Notify } from '../models/notify';
 
 @Component({
-  selector: 'app-scaffold',
-  templateUrl: './scaffold.component.html',
-  styleUrls: ['./scaffold.component.scss']
+	selector: 'app-scaffold',
+	templateUrl: './scaffold.component.html',
+	styleUrls: ['./scaffold.component.scss']
 })
 export class ScaffoldComponent {
-  constructor(
-    private router: Router,
-    private appService: AppService,
-    public notifyService: NotifyService,
-  ) {
-    if (this.notifies.length == 0) this.subscribeMsgs();
-  }
+	constructor(
+		private router: Router,
+		private appService: AppService,
+		public notifyService: NotifyService,
+	) {
+		if (this.notifies.length == 0) this.subscribeMsgs();
+	}
 
-  notifies: Notify[] = [];
-  routerLink = RouterLink;
-  keyword = '';
+	notifies: Notify[] = [];
+	routerLink = RouterLink;
+	keyword = '';
+	keyWordTags = {};
+	hotTags = []
 
-  newPlan() {
-    this.appService.getNew().then((id) => {
-      this.router.navigate([RouterLink.EditingPlan, id]);
-    });
-  }
+	ngOnInit() {
+		this.hotTags = ["年級", "科目"]
+		this.keyWordTags = this.hotTags.reduce((a, key) => Object.assign(a, { [key]: "" }), {});
+	}
 
-  onSearch(): void {
-    let isKeywordEmpty = this.keyword.trim().length == 0;
-    if (isKeywordEmpty) return;
 
-    this.router.navigate([RouterLink.Search], { queryParams: { q: this.keyword } });
-  }
+	newPlan() {
+		this.appService.getNew().then((id) => {
+			this.router.navigate([RouterLink.EditingPlan, id]);
+		});
+	}
 
-  resetKeyword() {
-    this.keyword = "";
-  }
+	getHotTags(): string[] {
+		return this.hotTags;
+	}
 
-  subscribeMsgs() {
-    this.notifyService.getMsgs().subscribe(notifies => this.notifies = notifies)
-  }
+	onSearch(): void {
+		console.log(this.keyWordTags);
+		let isKeywordEmpty = 
+			Object.values(this.keyWordTags).find(value => value !== "") === undefined &&
+			this.keyword.length==0;
+		if (isKeywordEmpty) return;
+
+		this.router.navigate([RouterLink.Search], { queryParams: { q: this.keyword } });
+	}
+
+	resetKeyword() {
+		this.keyword = "";
+	}
+
+	subscribeMsgs() {
+		this.notifyService.getMsgs().subscribe(notifies => this.notifies = notifies)
+	}
 }
